4fdfa631282c6829d50e2f9a47ae848fcc5e8c65
Dmitry <ankarin@Dmitrys-MacBook-Pro.local>
2023-10-06 19:26:18 +0300

    save

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,89 +1,91 @@
-'use server'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { Company, Developer, User } from '@/utils/types'
+'use server';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { Company, Developer, User } from '@/utils/types';
 
 const supa = async () => {
-  return createServerComponentClient({ cookies })
-}
+  'use server';
+  return createServerComponentClient({ cookies });
+};
 
 const getUser = async () => {
-  const supabase = await supa()
-  const { data } = await supabase.auth.getUser()
-  console.log(data)
+  'use server';
+  const supabase = await supa();
+  const { data } = await supabase.auth.getUser();
+  console.log(data);
   if (data.user) {
-    return data.user
+    return data.user;
   } else {
-    return null
+    return null;
   }
-}
+};
 
 const getUserData = async (): Promise<User> => {
-  const supabase = await supa()
-  const user = await getUser()
+  const supabase = await supa();
+  const user = await getUser();
   if (user) {
     const { data, error } = await supabase
       .from('users')
       .select()
-      .eq('id', user.id)
-    if (error) throw error.message
-    return data[0]
+      .eq('id', user.id);
+    if (error) throw error.message;
+    return data[0];
   } else {
-    return null
+    return null;
   }
-}
+};
 const getCompanyData = async (): Promise<Company> => {
-  const supabase = await supa()
-  const userData = await getUserData()
+  const supabase = await supa();
+  const userData = await getUserData();
   if (userData && userData.company_id) {
     const { data, error } = await supabase
       .from('companies')
       .select()
-      .eq('id', userData.company_id)
-    if (error) throw error.message
-    return data[0]
+      .eq('id', userData.company_id);
+    if (error) throw error.message;
+    return data[0];
   } else {
-    return null
+    return null;
   }
-}
+};
 
 const getCompanies = async (): Promise<Company[]> => {
-  const supabase = await supa()
-  const { data, error } = await supabase.from('companies').select()
-  if (error) throw error.message
-  return data
-}
+  const supabase = await supa();
+  const { data, error } = await supabase.from('companies').select();
+  if (error) throw error.message;
+  return data;
+};
 
 const getMyDevs = async (): Promise<Developer[]> => {
-  const supabase = await supa()
-  const userData = await getUserData()
+  const supabase = await supa();
+  const userData = await getUserData();
   const { data, error } = await supabase
     .from('developers')
     .select()
-    .eq('company', userData.company_id)
-  if (error) throw error.message
-  return data
-}
+    .eq('company', userData.company_id);
+  if (error) throw error.message;
+  return data;
+};
 const getDevs = async (): Promise<Developer[]> => {
-  const supabase = await supa()
-  const { data, error } = await supabase.from('developers').select()
-  if (error) throw error.message
-  return data
-}
+  const supabase = await supa();
+  const { data, error } = await supabase.from('developers').select();
+  if (error) throw error.message;
+  return data;
+};
 
 const getDev = async (devId): Promise<Developer | null> => {
-  const supabase = await supa()
+  const supabase = await supa();
   const { data, error } = await supabase
     .from('developers')
     .select()
-    .eq('id', devId)
-  if (error) return null
+    .eq('id', devId);
+  if (error) return null;
   if (data[0]) {
-    return data[0]
+    return data[0];
   } else {
-    return null
+    return null;
   }
-}
+};
 
 export {
   getUser,
@@ -93,4 +95,4 @@ export {
   getMyDevs,
   getDevs,
   getDev,
-}
+};
