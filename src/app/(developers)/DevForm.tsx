1f90cab7b4ca6e7dd1d82fa404f826fce72c5f83
Ankarin <ankarn41k@gmail.com>
2023-10-16 15:23:07 +0300

    fix dev form

--- src/app/(developers)/DevForm.tsx
+++ src/app/(developers)/DevForm.tsx
@@ -1,11 +1,11 @@
-'use client';
+'use client'
 
-import { uuid } from '@supabase/supabase-js/dist/main/lib/helpers';
-import { XMarkIcon, TrashIcon } from '@heroicons/react/24/solid';
-import React, { useEffect, useState } from 'react';
-import Select from 'react-select';
-import CreatableSelect from 'react-select/creatable';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { uuid } from '@supabase/supabase-js/dist/main/lib/helpers'
+import { XMarkIcon, TrashIcon } from '@heroicons/react/24/solid'
+import React, { useEffect, useState } from 'react'
+import Select from 'react-select'
+import CreatableSelect from 'react-select/creatable'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import {
   countries,
   englishLevels,
@@ -13,143 +13,145 @@ import {
   categorys,
   getRegion,
   skillList,
-} from '@/utils/options';
+} from '@/utils/options'
 
-import { selectStyleObject } from '@/utils/utils';
-import { zust } from 'src/store';
-import { toast } from 'react-toastify';
-import Toast from '@/components/app/Toast';
-import { useRouter } from 'next/navigation';
-import { supaDownload } from '@/utils/supabaseClient';
-import { Developer, Selecter } from '@/utils/types';
-import ConfirmDelete from '@/components/app/Confirm';
+import { selectStyleObject } from '@/utils/utils'
+import { zust } from 'src/store'
+import { toast } from 'react-toastify'
+import Toast from '@/components/app/Toast'
+import { useRouter } from 'next/navigation'
+import { supaDownload } from '@/utils/supabaseClient'
+import { Developer, Selecter } from '@/utils/types'
+import ConfirmDelete from '@/components/app/Confirm'
 
-const supabase = createClientComponentClient();
+const supabase = createClientComponentClient()
 
-export default function DevForm({ isNew = false, dev }: {
-  isNew: boolean;
-  dev: Developer | null; // Add null to the type
+export default function DevForm({
+  isNew = false,
+  dev,
+}: {
+  isNew: boolean
+  dev: Developer | null // Add null to the type
 }) {
-  const [title, setTitle] = useState<string>(''); // Add string type
-  const [category, setCategory] = useState<string>(''); // Add string type
-  const [experience, setExperience] = useState<string>('3'); // Add string type
-  const [location, setLocation] = useState<string>(''); // Add string type
-  const [asap, setAsap] = useState<boolean>(false); // Add boolean type
-  const [english, setEnglish] = useState<string>(''); // Add string type
-  const [rate, setRate] = useState<string>(''); // Add string type
-  const [otherLanguages, setOtherLanguages] = useState<Selecter[]>([]); // Add Selecter[] type
-  const [mainSkills, setMainSkills] = useState<Selecter[]>([]); // Add Selecter[] type
-  const [description, setDescription] = useState<string>(''); // Add string type
-  const [uploading, setUploading] = useState<boolean>(false); // Add boolean type
+  const [title, setTitle] = useState<string>('') // Add string type
+  const [category, setCategory] = useState<string>('') // Add string type
+  const [experience, setExperience] = useState<string>('3') // Add string type
+  const [location, setLocation] = useState<string>('') // Add string type
+  const [asap, setAsap] = useState<boolean>(false) // Add boolean type
+  const [english, setEnglish] = useState<string>('') // Add string type
+  const [rate, setRate] = useState<string>('') // Add string type
+  const [otherLanguages, setOtherLanguages] = useState<Selecter[]>([]) // Add Selecter[] type
+  const [mainSkills, setMainSkills] = useState<Selecter[]>([]) // Add Selecter[] type
+  const [description, setDescription] = useState<string>('') // Add string type
+  const [uploading, setUploading] = useState<boolean>(false) // Add boolean type
   const [cv, setCv] = useState<{
-    url: string;
+    url: string
     filename: string
   }>({
     url: '',
     filename: '',
-  }); // Add object type
-  const [disabledSave, setDisabledSave] = useState<boolean>(false); // Add boolean type
-  const [isPublic, setPublic] = useState<boolean>(true); // Add boolean type
-  const [file, setFile] = useState<File | null>(null); // Add File type
-  const router = useRouter();
+  }) // Add object type
+  const [disabledSave, setDisabledSave] = useState<boolean>(false) // Add boolean type
+  const [isPublic, setPublic] = useState<boolean>(true) // Add boolean type
+  const [file, setFile] = useState<File | null>(null) // Add File type
+  const router = useRouter()
 
-  const [openConfirm, setOpenConfirm] = useState<boolean>(false); // Add boolean type
+  const [openConfirm, setOpenConfirm] = useState<boolean>(false) // Add boolean type
 
   useEffect(() => {
     if (dev) {
-      setTitle(dev.title);
-      setCategory(dev.category);
-      setExperience(dev.experience);
-      setLocation(dev.country);
-      setAsap(dev.asap);
-      setEnglish(dev.english);
-      setRate(dev.hourly_rate);
+      setTitle(dev.title)
+      setCategory(dev.category)
+      setExperience(dev.experience)
+      setLocation(dev.country)
+      setAsap(dev.asap)
+      setEnglish(dev.english)
+      setRate(dev.hourly_rate)
       setOtherLanguages(
         dev.other_languages.map((item) => {
-          return { value: item, label: item };
-        }),
-      );
+          return { value: item, label: item }
+        })
+      )
 
       setMainSkills(
         dev.skills.map((item) => {
-          return { value: item, label: item };
-        }),
-      );
-      setDescription(dev.description);
-      setCv({ url: dev.cv.url, filename: dev.cv.name });
-      setPublic(dev.public);
+          return { value: item, label: item }
+        })
+      )
+      setDescription(dev.description)
+      setCv({ url: dev.cv.url, filename: dev.cv.name })
+      setPublic(dev.public)
     }
-  }, [dev]);
+  }, [dev])
 
-  const zustMyCompany = zust((state) => state.myCompany);
+  const zustMyCompany = zust((state) => state.myCompany)
 
   const languagesOptions = languages()
     .map((item) => {
-      return { value: item, label: item };
+      return { value: item, label: item }
     })
-    .filter((item) => !otherLanguages.includes(item));
+    .filter((item) => !otherLanguages.includes(item))
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const handleSelectOtherLanguages = (val) => {
-    console.log(val);
+    console.log(val)
     if (val.length > 5) {
-      return;
+      return
     }
-    setOtherLanguages(val);
-  };
+    setOtherLanguages(val)
+  }
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const handleSelectSkills = (val) => {
     if (val.length > 15) {
-      return;
+      return
     }
-    setMainSkills(val);
-  };
+    setMainSkills(val)
+  }
 
   async function loadCV(e: React.ChangeEvent<HTMLInputElement>) {
     if (!e.target.files || e.target.files.length === 0) {
-      throw new Error('You must select pdf to upload.');
+      throw new Error('You must select pdf to upload.')
     }
-    const file = e.target.files[0];
-    setFile(file);
-    const path = uuid();
+    const file = e.target.files[0]
+    setFile(file)
+    const path = uuid()
     setCv({
       url: `cv/${path}`,
       filename: file.name,
-    });
+    })
   }
 
   const supaAddDev = async (developer: Developer) => {
     const { data, error } = await supabase
       .from('developers')
       .insert(developer)
-      .select();
+      .select()
     if (error) {
-      toast.error('Error adding developer!');
+      toast.error('Error adding developer!')
     }
-    router.refresh();
+    router.refresh()
     if (data) {
-      router.push(`/edit-dev/${data[0].id}`);
-      toast.success('Added new developer!');
+      router.push(`/edit-dev/${data[0].id}`)
+      toast.success('Added new developer!')
     }
-  };
-
+  }
 
   const supaEditDev = async (developer: Developer) => {
     const { error } = await supabase
       .from('developers')
       .update(developer)
       .eq('id', dev?.id)
-      .select();
+      .select()
     if (error) {
-      toast.error('Error updating developer!');
-      return;
+      toast.error('Error updating developer!')
+      return
     } else {
-      toast.success('Updated developer!');
+      toast.success('Updated developer!')
     }
-    router.refresh();
-  };
+    router.refresh()
+  }
 
   const saveDeveloper = async () => {
     const developer: Developer = {
@@ -167,64 +169,64 @@ export default function DevForm({ isNew = false, dev }: {
       description,
       cv: { url: cv.url, name: cv.filename },
       public: isPublic,
-    };
-    if (isNew) {
-      await supaAddDev(developer);
-    } else {
-      await supaEditDev(developer);
     }
-  };
+    if (isNew) {
+      await supaAddDev(developer)
+    } else {
+      await supaEditDev(developer)
+    }
+  }
 
   async function uploadCv() {
-    if (dev && dev.cv.url === cv.url) return;
-    if (!file) return;
+    if (dev && dev.cv.url === cv.url) return
+    if (!file) return
 
     if (!cv.url && dev?.cv.url) {
-      await supabase.storage.from('bitbencher/').remove([dev.cv.url]);
+      await supabase.storage.from('bitbencher/').remove([dev.cv.url])
     }
 
     try {
-      setUploading(true);
+      setUploading(true)
       const { error: uploadError } = await supabase.storage
         .from('bitbencher/')
-        .upload(cv.url, file);
+        .upload(cv.url, file)
       if (uploadError) {
-        toast.error('Error uploading CV !');
+        toast.error('Error uploading CV !')
       }
     } catch (error) {
-      console.log(error);
-      return;
+      console.log(error)
+      return
     } finally {
-      setUploading(false);
+      setUploading(false)
     }
   }
 
   const save = async (e: React.FormEvent<HTMLFormElement>) => {
-    setDisabledSave(true);
-    e.preventDefault();
-    await uploadCv();
-    await saveDeveloper();
-    setDisabledSave(false);
-  };
+    setDisabledSave(true)
+    e.preventDefault()
+    await uploadCv()
+    await saveDeveloper()
+    setDisabledSave(false)
+  }
 
   const handlePublic = (state: boolean) => {
-    setPublic(state);
-  };
+    setPublic(state)
+  }
 
   const confirmDelete = async () => {
-    setOpenConfirm(false);
+    setOpenConfirm(false)
     const { error } = await supabase
       .from('developers')
       .delete()
-      .eq('id', dev?.id);
+      .eq('id', dev?.id)
     if (error) {
-      toast.error('Error deleting developer!');
-      console.log(error);
-      return;
+      toast.error('Error deleting developer!')
+      console.log(error)
+      return
     }
-    toast.success('Developer deleted!');
-    router.push('/my-devs');
-  };
+    toast.success('Developer deleted!')
+    router.push('/my-devs')
+  }
 
   return (
     <form onSubmit={save}>
@@ -233,8 +235,7 @@ export default function DevForm({ isNew = false, dev }: {
       <div className='space-y-12'>
         <div className='border-b border-gray-900/10 pb-12'>
           <div className={'items-center justify-between sm:flex'}>
-            <h2
-              className='pb-3 text-2xl font-bold leading-7 text-gray-900 sm:col-span-3 sm:truncate sm:p-0 sm:text-3xl sm:tracking-tight'>
+            <h2 className='pb-3 text-2xl font-bold leading-7 text-gray-900 sm:col-span-3 sm:truncate sm:p-0 sm:text-3xl sm:tracking-tight'>
               {isNew ? `Add Developer` : `Edit Developer`}
             </h2>
             <div className={'sm:col-span-3'}>
@@ -282,8 +283,7 @@ export default function DevForm({ isNew = false, dev }: {
               >
                 Title *
               </label>
-              <div
-                className='mt-2 flex rounded-md  ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600'>
+              <div className='mt-2 flex rounded-md  ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600'>
                 <input
                   required
                   maxLength={40}
@@ -309,7 +309,7 @@ export default function DevForm({ isNew = false, dev }: {
                 name='category'
                 value={category}
                 onChange={(e) => {
-                  setCategory(e.target.value);
+                  setCategory(e.target.value)
                 }}
                 className='mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6'
               >
@@ -332,7 +332,7 @@ export default function DevForm({ isNew = false, dev }: {
                 name='location'
                 value={`${location}`}
                 onChange={(e) => {
-                  setLocation(e.target.value);
+                  setLocation(e.target.value)
                 }}
                 className='mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6'
               >
@@ -359,7 +359,7 @@ export default function DevForm({ isNew = false, dev }: {
                   name='english'
                   value={english}
                   onChange={(e) => {
-                    setEnglish(e.target.value);
+                    setEnglish(e.target.value)
                   }}
                   className='mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 >
@@ -421,7 +421,7 @@ export default function DevForm({ isNew = false, dev }: {
                   max='10'
                   value={experience}
                   onChange={(e) => {
-                    setExperience(e.target.value);
+                    setExperience(e.target.value)
                   }}
                   list='markers'
                   className='light h-2  w-full cursor-pointer appearance-none rounded-lg bg-gray-200 dark:bg-gray-700'
@@ -526,8 +526,8 @@ export default function DevForm({ isNew = false, dev }: {
                   </span>
                   <XMarkIcon
                     onClick={() => {
-                      setFile(null);
-                      setCv({ url: '', filename: '' });
+                      setFile(null)
+                      setCv({ url: '', filename: '' })
                     }}
                     className='ml-5 h-6 w-6 cursor-pointer text-red-500'
                   ></XMarkIcon>
@@ -535,53 +535,42 @@ export default function DevForm({ isNew = false, dev }: {
               )}
             </div>
             <div className={'sm:col-span-3'}>
-              <label
-                htmlFor='cover-photo'
-                className='block text-sm font-medium leading-6 text-gray-900'
-              >
-                Delete Developer
-              </label>
-              <div className='mt-2'>
-                <div className='grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6'>
-                  <button
-                    onClick={() => setOpenConfirm(true)}
-                    type='button'
-                    className='block w-full rounded-md py-2 px-3 text-base font-medium bg-red-700 hover:bg-red-800 text-white transition-colors duration-150'
-                  >
-                    Delete Developer
-                  </button>
-                </div>
+              <div className='flex items-center gap-x-3'>
+                <input
+                  id='available'
+                  name='available'
+                  type='radio'
+                  checked={asap}
+                  onChange={() => setAsap(true)}
+                  className='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'
+                />
+                <label
+                  htmlFor='push-everything'
+                  className='block text-sm leading-6 text-gray-900'
+                >
+                  <span className={'font-semibold'}>ASAP - </span> Ready to
+                  start within 2 days.
+                </label>
+              </div>
+
+              <div className='flex  items-center gap-x-3'>
+                <input
+                  id='push-email'
+                  name='available'
+                  type='radio'
+                  checked={!asap}
+                  onChange={() => setAsap(false)}
+                  className='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600/60'
+                />
+                <label
+                  htmlFor='push-email'
+                  className='block text-sm  leading-6 text-gray-900'
+                >
+                  Ready to start soon.
+                </label>
               </div>
             </div>
           </div>
-        </div>
-        <div className='space-x-2'>
-          <button
-            type='submit'
-            className={`${
-              disabledSave
-                ? 'cursor-not-allowed'
-                : 'cursor-pointer text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-            } group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md transition duration-150 ease-in-out`}>
-            {uploading ? (
-              <svg
-                className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'>
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M12 4v16m8-8H4'
-                />
-              </svg>
-            ) : (
-              ''
-            )}
-            Save
-          </button>
         </div>
       </div>
       <ConfirmDelete
@@ -621,5 +610,5 @@ export default function DevForm({ isNew = false, dev }: {
         </div>
       </div>
     </form>
-  );
+  )
 }
